07e1772196ac90492bb7d7b01aa665f97f43f597
Adam Wagner <adam@copilotlabs.com>
2013-04-16 23:14:15 -0700

    Backbone setup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,44 @@
-/* @codekit-prepend "lib/angular.js" */
 /* @codekit-prepend "data.js" */
 
+
+
+var AlbumModel = Backbone.Model.extend({
+
+  initialize: function() {
+
+  }
+});
+
+
+
+var albumModel= new AlbumModel(albums[0]);
+
+console.log(albumModel.attributes);
+
+
+var AlbumView = Backbone.View.extend({
+
+  tag : 'p',
+
+  template : _.template( '<p> <%= this.model.albumName %> </p>' ),
+
+  events: {
+
+  },
+
+  initialize: function() {
+
+  },
+
+  render: function() {
+    this.$el.html( this.template( this.model.toJSON() ) );
+    return this;
+  }
+});
+
+
+var albumView = new AlbumView({ model: albumModel});
+
+console.log(albumView.render().el);
+
+// $('html').append(albumView);
--- parts/content-loops/album-loop.php
+++ parts/content-loops/album-loop.php
@@ -1,26 +1,17 @@
 
 
 
-<div ng-controller="AlbumCtrl">
 
-<div ng-click="play(album)" ng-repeat="album in albums | filter:{kind:'album'}" class="grid album {{ album.isPlaying }}">
+<div class="grid album">
    <div class="album-cover playable">
      <div class="play"></div>
      <div class="pause"></div>
-     <div class="album-cover-img lazyload fade ratio-1-1" data-src="{{album.coverUrl}}" alt="{{album.albumName + album.artist}}">
-     </div>
-   </div>
+</div>
    <a class="link-to-album" href="<?php echo the_permalink(); ?>">
        <div class="album-meta">
-         <span data-icon="s" class="speaker-indicator"></span>
-         <div class="album-title">{{album.albumName}}</div>
-         <div class="artist-name">{{album.artist}}</div>
+         <script id="album" type="template">
+            <div> <%= album.albumName %> </div>
+         </script>
        </div>
    </a>
- </div>
-
-
-
-
-
 </div>
