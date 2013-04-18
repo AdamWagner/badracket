3b1487e619e2c32f94b207fd87fc1cd4e15b2c30
Adam Wagner <adam@copilotlabs.com>
2013-04-17 17:00:28 -0700

    Global structure

--- js/album-page.js
+++
@@ -1,8 +0,0 @@
-badracket.loadBandTweets = function(){
-  var twitterHandle = $('.album-page-wrapper').attr('data-twitter');
-  if (twitterHandle.substring(0,1) === '@') {
-    twitterHandle = twitterHandle.substring(1);
-  }
-  JQTWEET.user = twitterHandle;
-  JQTWEET.loadTweets(3);
-};
