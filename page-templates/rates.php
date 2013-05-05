614dd04f9af31269dd18967b2053bebc954fdbb9
Adam Wagner <adam@copilotlabs.com>
2013-05-04 17:06:53 -0700

    Merging untracked files

---
+++ page-templates/rates.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Template Name: Rates
+ * @package   WordPress
+ * @subpackage  Starkers
+ * @since     Starkers 4.0
+ */
+?>
+
+<?php get_template_part('parts/clusters/page-bootstrap-top'); ?>
+
+<section class="pink-cream bottom1 padded-mobile-1">
+
+  <div class="row s-1 m-3 group pt1 pb2 pricing">
+
+    <h2 class="bottom1-5 top1">Tracking &amp; Mixing</h2>
+
+    <div class="grid">
+      <header>
+        <h3 class="bottom0">SHORT DAY:</h3>
+        <div class="subtitle">up to 3 hr</div>
+      </header>
+      <div class="price">$135</div>
+      <a class="button mailto" href="mailto:thomas@badracket.com?subject=I just need a short session&amp;body=Describe what your project in as much detail as possible, and we'll be in touch.  Thanks!">Schedule short day</a>
+    </div>
+
+    <div class="grid border-left">
+      <header>
+        <h3 class="bottom0">DAY:</h3>
+        <div class="subtitle">up to 6 hrs</div>
+      </header>
+      <div class="price">$270</div>
+      <a class="button mailto" href="mailto:thomas@badracket.com?subject=I need to book a regular day session&amp;body=Describe what your project in as much detail as possible, and we'll be in touch.  Thanks!">Schedule regular day</a>
+    </div>
+
+    <div class="grid border-left">
+      <header>
+        <h3 class="bottom0">LONG DAY:</h3>
+        <div class="subtitle">up to 10 hrs</div>
+      </header>
+      <div class="price">$450</div>
+      <a class="button mailto" href="mailto:thomas@badracket.com?subject=I need to book a long day session or longer&amp;body=Describe what your project in as much detail as possible, and we'll be in touch.  Thanks!">Schedule long day</a>
+    </div>
+
+  </div> <!-- end row -->
+
+  <div class="row pt1 group">
+
+    <h2 class="bottom05">Mastering</h2>
+    <p class="bottom05"><em>Will</em> we master your project? Yes.  It's $15 a song, and sounds pretty great.</p>
+    <p>But we'll be straight with you: We're not a mastering studio.  You'll walk out the door with ready-to-share tracks, but they'll be even more awesome if you get a dedicated mastering engineer to massage your tracks one last time.  We know some people.  Ask us for a recommendation.
+
+  </div> <!-- end row -->
+
+
+  <div class="row group">
+
+    <h2 class="bottom05">Video</h2>
+    <p>All video projects are custom-quoted.  Email <a class="mailto" href="mailto:thomas@badracket.com?subject=I need a video&amp;body=Describe what your project in as much detail as possible, and we'll be in touch.  Thanks!">Thomas</a> with a description of what you want to make &amp; he'll get back to ya.</p>
+
+  </div> <!-- end row -->
+
+  <div class="row ab1 group">
+
+    <h2 class="bottom05">Freelance Rates</h2>
+    <p>We love opening up our live room to freelance engineers, or a band who already has an engineer they like to work with.  Our freelance rates are totally fair. <a href="mailto:thomas@badracket.com?subject=I'd like to rent the studio, but I already have (or am) an engineer&amp;body=Describe what your project in as much detail as possible, and we'll be in touch.  Thanks!">Send us an email</a> describing your plans, and we'll hook you up.</p>
+
+  </div> <!-- end row -->
+
+</section>
+
+
+<section class="orange first specs-light island-huge faq padded-mobile-1">
+
+  <div class="row">
+    <h2>FAQ</h2>
+    <ul class="top1">
+      <li>
+        <p class="bottom0"><strong>Do you offer time by the hour?</strong></p>
+        <p>No, we don't.  What could you get done in an hour? Don't answer that... the answer is "not much".  Working in day-chunks is a more natural way to think of projects.  You'll see.</p>
+      </li>
+      <li>
+        <p class="bottom0"><strong>What do do the rates include?</strong></p>
+        <p>Pretty much everything.  Space, engineer, all of our gear, a Wendy's run, the beer fridge, and mixing. </p>
+      </li>
+      <li>
+        <p class="bottom0"><strong>Can you give me a custom quote?</strong></p>
+        <p>Yea, we do it all the time.  Send us a <a href="mailto:thomas@badracket.com?subject=I'd like a custom quote&amp;body=Describe what your project in as much detail as possible, and we'll be in touch.  Thanks!">detailed email</a> about your project, and we'll give you a price.</p>
+      </li>
+    </ul>
+  </div>
+
+</section>
+
+<?php get_template_part('parts/clusters/page-bootstrap-bottom'); ?>
