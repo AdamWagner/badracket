c6ebd0659c048972c482066f2dc2e29bcce3bd3e
AdamWagner <adam@copilotlabs.com>
2013-05-11 15:31:38 -0700

    Migration to WPEngine

---
+++ fabfile.py
@@ -0,0 +1,114 @@
+from __future__ import with_statement
+from fabric.api import *
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+import hashlib
+import gzip
+import os
+
+# Constants
+WP_PATH = 'wp-content/themes/starkers'
+
+# bucket: copilot-fabric-test
+ACCESS_KEY = 'AKIAJUBYJCMVA5PFFJKA'
+SECRET = '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'
+STAGE_BUCKET_NAME = 'copilot-concourse-staging-assets'
+PROD_BUCKET_NAME = 'copilot-concourse-assets'
+
+
+def make_filepath(filename, subdirectory):
+    return WP_PATH + subdirectory + '/' + filename
+
+
+def md5_for_filename(filepath, block_size=2 ** 20):
+    md5 = hashlib.md5()
+    fh = open(filepath, 'rb')
+    while True:
+        data = fh.read(block_size)
+        if not data:
+            break
+        md5.update(data)
+    fh.close()
+    return md5.hexdigest()
+
+
+def versioned_filename_builder(filename, subdirectory):
+    filename_parts = filename.split('.')
+    extension = filename_parts[1]
+    basename = filename_parts[0]
+
+    filepath = make_filepath(filename, subdirectory)
+    return '%s.%s.%s.%s' % (basename, md5_for_filename(filepath), 'gzip', extension)
+
+
+def gzip_assets(filename, subdirectory, env):
+    print '**** gzipping ' + filename + '****'
+    filepath = make_filepath(filename, subdirectory)
+
+    filename_parts = filename.split('.')
+    extension = filename_parts[1]
+    basename = filename_parts[0]
+
+    class FakeTime:
+        def time(self):
+            return 1261130520.0
+
+    # Hack to override gzip's time implementation
+    # !! This line made the gzipping NOT decode in the browser.
+    # gzip.time = FakeTime()
+
+    gzipped_file_path = WP_PATH + subdirectory + '/' + basename + '.gzip.' + extension
+
+    f_in = open(filepath, 'rb')
+    contents = f_in.readlines()
+    f_in.close()
+    f_out = gzip.open(gzipped_file_path, 'wb')
+    f_out.writelines(contents)
+    f_out.close()
+
+    save_file_in_s3(filename, subdirectory, env, gzipped_file_path)
+
+
+def save_file_in_s3(filename, subdirectory, env, gzipped_file_path):
+    if env == 'staging':
+        BUCKET_NAME = STAGE_BUCKET_NAME
+    else:
+        BUCKET_NAME = PROD_BUCKET_NAME
+
+    versioned_filename = versioned_filename_builder(filename, subdirectory)
+
+    if subdirectory != '':
+        remote_filepath = WP_PATH + subdirectory + '/'
+    else:
+        remote_filepath = WP_PATH + '/'
+
+    print 'uploading -- %s --  to -- %s --' % (versioned_filename, BUCKET_NAME + ' : ' + remote_filepath)
+
+    #set headers
+    # css -> content-type: text/css, content-encoding: gzip
+    # js -> content-type: application/javascript, content-encoding: gzip
+
+    conn = S3Connection(ACCESS_KEY, SECRET)
+    bucket = conn.get_bucket(BUCKET_NAME)
+    k = Key(bucket)
+    k.key = remote_filepath + versioned_filename
+    k.set_metadata('Content-Encoding', 'gzip')
+    k.set_contents_from_filename(gzipped_file_path)
+    k.make_public()
+
+    print '**** Deleting ' + gzipped_file_path + '****'
+    os.remove(gzipped_file_path)
+
+
+def deploy(env):
+    print '**** Uploading to S3 ****'
+    gzip_assets('style.css', '', env)
+    gzip_assets('demo-account.css', '/css', env)
+    gzip_assets('site-min.js', '/js', env)
+    gzip_assets('homepage-min.js', '/js', env)
+    print '**** pushing to wp %s ****' % env
+    if env == 'staging':	
+       local('git push staging staging')
+    else:
+       local('git push production master')
+          
--- index.php
+++ index.php
@@ -1,35 +1,17 @@
 <?php
 /**
- * The main template file
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file 
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
  *
- * Please see /external/starkers-utilities.php for info on get_template_parts()
- *
- * @package 	WordPress
- * @subpackage 	Starkers
- * @since 		Starkers 4.0
+ * @package WordPress
  */
-?>
-<?php get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
-<?php if ( have_posts() ): ?>
-<h2>Latest Posts</h2>	
-<ol>
-<?php while ( have_posts() ) : the_post(); ?>
-	<li>
-		<article>
-			<h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-			<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
-			<?php the_content(); ?>
-		</article>
-	</li>
-<?php endwhile; ?>
-</ol>
-<?php else: ?>
-<h2>No posts to display</h2>
-<?php endif; ?>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
 
-<?php get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer') ); ?>
+/** Loads the WordPress Environment and Template */
+require('./wp-blog-header.php');
