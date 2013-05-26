8bcbeedd51d40e8f19b64fbe6eb53e9b0b3da691
AdamWagner <adam@copilotlabs.com>
2013-05-25 23:48:06 -0700

    Added homepage illustrations and copy

--- wp-content/themes/badracket/page-templates/homepage.php
+++ wp-content/themes/badracket/page-templates/homepage.php
@@ -48,11 +48,17 @@
        <div class="album-name pull-left padded-sides">Record</div>
        <a href="<?php echo get_page_link(9);?>" class="pill-button see-albums pull-right padded-sides">Explore our process&nbsp <span>▶</span></a>
      </div>
-       <div class="txt-col-2 padded-sides limit-1">
-       <p>We love seeing music live, when people get together and pour out lyrics over chords into a room full of strangers. There’s something essential in four walls that makes music sound so good. Our space is inspired by that idea. It’s not too fancy: a single acoustically treated live room with lots of space to play, a team of people that live for sound, and the tools to make silent smiles.</p>
-       <p>We’ve also been lucky to record some great artists. Their talent makes this whole thing worthwhile.</p>
-       <p>We’re accepting new projects.</p>
-       <p>Hours are from noon to 12am Monday-Saturday, 6pm-12pm Sunday. You can find more information on rates here, give us a call, or schedule a time to come over and take a tour of the studio.</p>
+     <div class="group s-1 m-2">
+       <div class="grid padded-sides-2">
+         <img class="recording-process-image lazyload_img" src="http://d245myou62vn42.cloudfront.net/wp-content/uploads/2013/05/recording-process.jpg" alt="Recording studio, Cleveland style">
+       </div>
+       <div class="grid padded-sides limit-1 shortform-text">
+         <p>A recording studio is first and foremost a performance space, and that’s where we begin. Although getting tones with fascinating audio gear and tweaking levels to make great sound excites our <a href="<?php echo get_page_link(2476);?>">inner nerd</a>, recordings are ultimately a capture of your performance. </p>
+         <p>We’re very lucky to have an amazing <a href="<?php echo get_post_type_archive_link( 'staff' ); ?>">team of engineers</a> and even more fortunate to have the culture, energy, and talent of the Cleveland music community at our back. </p>
+         <p><a href="mailto:play@badracket.com">Book studio time</a> by the day or a short day, not on the hour, and spend some time emerged in the music. We’ll take care of the rest. </p>
+         <p>Learn more about our team and our <a href="<?php echo get_page_link(9);?>">recording process</a>, and get in touch with us to schedule a session.</p>
+         
+       </div>
      </div>
 
 
@@ -63,16 +69,18 @@
   <section class="orange pb3">
 
      <div class="section-subtitle album-container-header group border-bottom">
-       <div class="album-name pull-left padded-sides">Team</div>
-       <a href="<?php echo get_post_type_archive_link( 'staff' ); ?>" class="pill-button see-albums pull-right padded-sides">Staff profiles &nbsp; <span>▶</span></a>
+       <div class="album-name pull-left padded-sides">Community</div>
+       <a href="<?php echo get_post_type_archive_link( 'staff' ); ?>" class="pill-button see-albums pull-right padded-sides">Team profiles &nbsp; <span>▶</span></a>
      </div>
-     <div class=" group p-5-9 s-3 m-5 b-5 h-6 padded">
-      <?php get_template_part('parts/content-loops/staff-loop');?>
-      <div class="txt-col-2 padded-sides limit-1">
-       <p>We love seeing music live, when people get together and pour out lyrics over chords into a room full of strangers. There’s something essential in four walls that makes music sound so good. Our space is inspired by that idea. It’s not too fancy: a single acoustically treated live room with lots of space to play, a team of people that live for sound, and the tools to make silent smiles.</p>
-       <p>We’ve also been lucky to record some great artists. Their talent makes this whole thing worthwhile.</p>
-       <p>We’re accepting new projects.</p>
-       <p>Hours are from noon to 12am Monday-Saturday, 6pm-12pm Sunday. You can find more information on rates here, give us a call, or schedule a time to come over and take a tour of the studio.</p>
+     <div class="group s-1 m-2">
+       <div class="grid padded-sides limit-1 shortform-text">
+         <p>Music Fans, you can find lots of <a href="<?php echo get_post_type_archive_link( 'album' ); ?>">music</a> and <a href="<?php echo get_page_link(160);?>">videos</a> here at Bad Racket. Musicians there’s ton’s of toys and time to make some awesome recordings, shows, videos with our team of recording engineers.</p> 
+         <p>The Cleveland music community works together in a simple, amazing way. Thanks to you for being a part of it. </p>
+         
+       </div>
+       <div class="grid padded-sides-2">
+         <img class="recording-process-image lazyload_img" src="http://d245myou62vn42.cloudfront.net/wp-content/uploads/2013/05/community.jpg" alt="Recording studio, Cleveland style">
+       </div>
      </div>
 
 
