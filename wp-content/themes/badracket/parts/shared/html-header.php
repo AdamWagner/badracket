0e53d76fe99a13ef6b50a878e86359a3c6ae31e9
AdamWagner <adam@copilotlabs.com>
2013-05-26 15:37:11 -0700

    Added favicon

--- wp-content/themes/badracket/parts/shared/html-header.php
+++ wp-content/themes/badracket/parts/shared/html-header.php
@@ -5,6 +5,7 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1 maximum-scale=1">
+  <link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.png" />
 
   <!-- <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" /> -->
   <!-- <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/> -->
