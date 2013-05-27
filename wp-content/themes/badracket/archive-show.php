b4755b14095568edc7a94ef5abf4f6167d2755d0
AdamWagner <adam@copilotlabs.com>
2013-05-26 18:52:06 -0700

    Add dollar sign to shows, increase h1 line height in longform text

--- wp-content/themes/badracket/archive-show.php
+++ wp-content/themes/badracket/archive-show.php
@@ -64,7 +64,7 @@
               <span data-fb-id="<?php echo $fb_event_id; ?>"  class="show-rsvp not-attending"><span data-icon="c" class="icon-checkmark"></span><span class="text">RSVP</span></span>
               <a href="#" class="show-buy-tickets top1">Get tickets</a>
               <div class="sub-button-details">
-                <div class="txtC top1">Tickets <?php echo $advance_ticket_price; ?> advance</div>
+                <div class="txtC top1">Tickets <?php echo '$' . $advance_ticket_price; ?> advance</div>
                 <div class="txtC top05">Show at <?php echo $show_human_time; ?></div>
               </div>
             </div>
