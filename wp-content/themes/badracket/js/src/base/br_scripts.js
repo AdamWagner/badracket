216abf0262624b1947b6d7051bee2ef1450c0987
AdamWagner <adam@copilotlabs.com>
2013-06-01 00:52:53 -0700

    Reference to payments fix

--- wp-content/themes/badracket/js/src/base/br_scripts.js
+++ wp-content/themes/badracket/js/src/base/br_scripts.js
@@ -8,7 +8,7 @@ br_scripts = function(){
 
 	return {
 		payments : cdn + wpPath + '/js/build/26854135.payments.min.js',
-		player : cdn + wpPath + '/js/build/fc421d39.br_audio-player.min.js',
+		player : cdn + wpPath + '/js/build/fc61ff38.br_audio-player.min.js',
 		facebook : cdn + wpPath + '/js/build/b506d51d.br_facebook.min.js',
 		postLoad : cdn + wpPath + '/js/build/c54d4052.post-load.min.js'
 	};
